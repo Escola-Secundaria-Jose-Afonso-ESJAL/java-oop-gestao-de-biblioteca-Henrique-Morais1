d793d70d82df6c81bad9f53a8474098b29dc140a
LightDark <goldenlightdark1st@gmail.com>
2026-02-11 15:17:16 +0000

    Realização do exercicio.

--- src/main/java/com/example/Biblioteca.java
+++ src/main/java/com/example/Biblioteca.java
@@ -1,4 +1,4 @@
-package  com.example;
+package com.example;
 
 import java.util.ArrayList;
 
@@ -11,14 +11,37 @@ public class Biblioteca {
     }
 
     public boolean adicionarLivro(Livro livro) {
+        if (livros.size() >= MAX_LIVROS) {
+            return false;
+        }
         
+        for (Livro l : livros){
+            if(livro.getTitulo().equals(livro.getTitulo())){
+                return false;
+            }
+        }
+        
+        return livros.add(livro);
     }
 
     public boolean removerLivro(String titulo) {
+        for (int i = 0; i < livros.size(); i++) {
+            if (livros.get(i).getTitulo().equals(titulo)) {
+                livros.remove(i);
+                return true;
+            }
+        }
         
+        return false;
     }
-
+    
     public void listarLivros() {
-        
+        for (int i = 0; i < livros.size(); i++) {
+            Livro livro = livros.get(i);
+            System.out.println(i + " - " + livro.getTitulo() +
+                " escrito por " + livro.getAutor() +
+                " no ano " + livro.getAno()
+            );
+        }
     }
 }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,25 @@
 package com.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        Biblioteca biblioteca1 = new Biblioteca();
         
+        Livro l1 = new Livro("O Senhor dos Anéis", "J.R.R. Tolkien", 1954);
+        Livro l2 = new Livro("Duna", "Frank Herbert", 1965);
+        Livro l3 = new Livro("1984", "George Orwell", 1949);
+        
+        ArrayList<Livro> livros = new ArrayList();
+        livros.add(l1);
+        livros.add(l2);
+        livros.add(l3);
+        
+        Livro l4 = new Livro("Duna", "Frank Herbert", 1965);
+        livros.add(l4);
+        
+        biblioteca1.removerLivro("1984");
+        
+        biblioteca1.listarLivros();
     }
 }
